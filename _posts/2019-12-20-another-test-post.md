7022f9ced16b7b68c737f676794e5b422640149d
crecker2 <joshua.fayer@gmail.com>
2019-12-21 22:28:01 -0500

    launch!

--- _posts/2019-12-20-another-test-post.md
+++ _posts/2019-12-20-another-test-post.md
@@ -1,6 +1,8 @@
 ---
 layout: post
 title: this is another test post
+img: banner.jpg
+alt: This is some sample alt text
 brief: This is a sample brief of a blog post. This is a sample brief of a blog post This is a sample brief of a blog postThis is a sample brief of a blog postThis is a sample brief of a blog postThis is a sample brief of a blog post.
 author: Josh Fayer
 type: "blog post"
--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -1,27 +1,27 @@
 $(document).ready(function(){
-    if(location.origin != "http://localhost:4000"){
-      if(!location.href.match(/construction/)){
-        if(!getCookie("developer")) {
-          location.href = "/construction";
-        }
-      }
-    }
+    // if(location.origin != "http://localhost:4000"){
+    //   if(!location.href.match(/construction/)){
+    //     if(!getCookie("developer")) {
+    //       location.href = "/construction";
+    //     }
+    //   }
+    // }
     $('.sidenav').sidenav();
 });
 
-
-function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i <ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
+// 
+// function getCookie(cname) {
+//   var name = cname + "=";
+//   var decodedCookie = decodeURIComponent(document.cookie);
+//   var ca = decodedCookie.split(';');
+//   for(var i = 0; i <ca.length; i++) {
+//     var c = ca[i];
+//     while (c.charAt(0) == ' ') {
+//       c = c.substring(1);
+//     }
+//     if (c.indexOf(name) == 0) {
+//       return c.substring(name.length, c.length);
+//     }
+//   }
+//   return "";
+// }
